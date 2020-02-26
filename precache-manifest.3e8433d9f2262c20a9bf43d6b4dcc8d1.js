982182245ee8db59ee172daf3c660981d619b5ae
kdhumphries <thekevindhumphries@gmail.com>
2020-02-26 09:31:15 -0600

    Updates

---
+++ precache-manifest.3e8433d9f2262c20a9bf43d6b4dcc8d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee6fd61aec1da55dc6c9",
+    "url": "/static/js/main.613971f2.chunk.js"
+  },
+  {
+    "revision": "2b3fc96d364091873d35",
+    "url": "/static/js/2.a5548ea4.chunk.js"
+  },
+  {
+    "revision": "ee6fd61aec1da55dc6c9",
+    "url": "/static/css/main.ab52ee6e.chunk.css"
+  },
+  {
+    "revision": "2b3fc96d364091873d35",
+    "url": "/static/css/2.3a72fb54.chunk.css"
+  },
+  {
+    "revision": "1d69071c4e8e8c3653c9e67e5263ea7f",
+    "url": "/index.html"
+  }
+];
